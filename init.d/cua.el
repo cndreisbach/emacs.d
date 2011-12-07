d3938c5e369634136ae3e6f498d426a4b16b49de
Clinton R. Nixon <crnixon@gmail.com>
2011-12-06 21:31:23 -0500

    Rectangle editing with cua-mode

---
+++ init.d/cua.el
@@ -0,0 +1,5 @@
+;; Cua-mode gives us superior rectangle editing.
+;; It also gives us Windows-like key commands (C-x/C-c/C-v), which I don't
+;; want, so I disable that.
+(setq cua-enable-cua-keys nil)
+(cua-mode)
