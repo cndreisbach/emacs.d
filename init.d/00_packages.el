92e7afe816bf565a6f08b260cd1950e5c2776f87
Clinton R. Nixon <crnixon@gmail.com>
2011-12-16 13:23:52 -0500

    Added packages

--- init.d/00_packages.el
+++ init.d/00_packages.el
@@ -1,30 +1,34 @@
 ;;; Load packages
-(setq my-packages '(
-		    anything anything-config
-		    coffee-mode
-		    cperl-mode
-		    deft
-		    feature-mode
-		    find-file-in-project
-		    full-ack
-		    go-mode
-		    haml-mode
-		    htmlize
-		    ido-ubiquitous
-		    magit
-		    markdown-mode
-		    marmalade
-		    sass-mode scss-mode
-		    smex
-		    undo-tree
-		    yaml-mode
-		    ))
+(setq my-packages '(anything
+                    anything-config
+                    auto-indent-mode
+                    coffee-mode
+                    cperl-mode
+                    deft
+                    feature-mode
+                    find-file-in-project
+                    full-ack
+                    go-mode
+                    haml-mode
+                    htmlize
+                    ido-ubiquitous
+                    magit
+                    markdown-mode
+                    marmalade
+                    org2blog
+                    sass-mode
+                    scss-mode
+                    smex
+                    undo-tree
+                    yaml-mode
+                    xml-rpc))
 
-(setq package-archives 
-      '(("marmalade" . "http://marmalade-repo.org/packages/") 
-	("gnu" . "http://elpa.gnu.org/packages/") 	
-	("Tromey" . "http://tromey.com/elpa/")))
+(setq package-archives
+      '(("marmalade" . "http://marmalade-repo.org/packages/")
+        ("gnu" . "http://elpa.gnu.org/packages/")
+        ("Tromey" . "http://tromey.com/elpa/")))
 
+(load "package")
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
