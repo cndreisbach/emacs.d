4a79babb574c8f263448f7bcbfaefe2927be3ca5
Clinton R. Nixon <crnixon@gmail.com>
2012-03-12 16:18:28 -0400

    update themes

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-safe-themes (quote ("e254937cba0f82c2d9eb3189a60748df9e486522" "1440d751f5ef51f9245f8910113daee99848e2c0" "485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" default)))
+ '(custom-safe-themes (quote ("54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "e254937cba0f82c2d9eb3189a60748df9e486522" "1440d751f5ef51f9245f8910113daee99848e2c0" "485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" default)))
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
  '(mark-even-if-inactive nil)
