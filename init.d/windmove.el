8126f1fe5ef28829b67c9af7700cb6953aa1eada
Clinton R. Nixon <crnixon@gmail.com>
2012-03-21 10:09:18 -0400

    Added windmove

---
+++ init.d/windmove.el
@@ -0,0 +1,5 @@
+(windmove-default-keybindings)
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
