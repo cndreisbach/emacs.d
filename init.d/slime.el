14dc97d1dd7b030757bcf29c4777ba2cb4933890
Clinton R. Nixon <crnixon@gmail.com>
2012-08-15 08:59:13 -0400

    merge commit

--- init.d/slime.el
+++ init.d/slime.el
@@ -1,2 +1,2 @@
-;(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;(setq inferior-lisp-program "sbcl")
+;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;;(setq inferior-lisp-program "sbcl")
