9ff3361c8388c52de6d9eec497edab7dc8fff8d2
Clinton R. Nixon <crnixon@gmail.com>
2012-08-22 22:12:57 -0400

    Add lisp-power-mode
    Convert my settings for all lisp modes to a minor mode that I can edit the keymap for.

--- init.d/lisp.el
+++ init.d/lisp.el
@@ -4,12 +4,21 @@
                    scheme-mode
                    clojure-mode))
 
-(defun cnd/engage-lisp-power ()
-  (paredit-mode 1)
+(defvar lisp-power-map (make-keymap))
+(define-minor-mode lisp-power-mode "Fix keybindings; add power."
+  :lighter " (power)"
+  :keymap lisp-power-map
+  (enable-paredit-mode)
   (show-paren-mode 1))
+(define-key lisp-power-map [delete] 'paredit-forward-delete)
+(define-key lisp-power-map [backspace] 'paredit-backward-delete)
+
+(defun cnd/engage-lisp-power ()
+  (lisp-power-mode t))
 
 (dolist (mode lisp-modes)
-  (add-hook (intern (concat (symbol-name mode) "-hook"))            
+  (add-hook (intern (concat (symbol-name mode) "-hook"))  
             #'cnd/engage-lisp-power))
 
 (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
+
