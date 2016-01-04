b6f0798ef60a6b05ce6206114f4d63e7f2139db1
Clinton N. Dreisbach <cdreisbach@rti.org>
2016-01-04 09:34:29 -0500

    Added racket mode

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 ;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
 ;; embedded in literate Org-mode files.
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "start.org" user-emacs-directory))
